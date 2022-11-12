0f03bff76979fa90053f31b5e6a85a8bc350ef10
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-11-12 15:02:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## php-test-module
+
+PHP модуль для проведения тестов.
+
+## Автор
+
+Shamanin Alexandr (@slpAkkie)
+
+## Version
+
+1.0.0
---
+++ options.php
@@ -0,0 +1,6 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+ini_set('html_errors', true);
